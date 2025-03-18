e2f3c70f16ca06fd8693c1ca42d4a55880c60319
Gabriel Revelo <gabriel.revelo@stockviz.com>
2025-03-17 21:53:39 -0500

    Refactor math operations into a single route and update templates for improved structure

--- app.py
+++ app.py
@@ -6,50 +6,25 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/suma', methods=['GET', 'POST'])
-def suma():
-    result = None
-    if request.method == 'POST':
-        dato1 = request.form.get('dato1')
-        dato2 = request.form.get('dato2')
-        if dato1 and dato2:
-            try:
-                num1 = float(dato1)
-                num2 = float(dato2)
+@app.route('/operacion', methods=['POST'])
+def operacion():
+    dato1 = request.form.get('dato1')    
+    dato2 = request.form.get('dato2')
+    action = request.form.get('action')
+    result = "resultados acá"
+    if dato1 and dato2:
+        try:
+            num1 = float(dato1)
+            num2 = float(dato2)
+            if action == 'sumar':
                 result = num1 + num2
-            except ValueError:
-                result = "ingresar solo numeros"
-    return render_template('suma.html', result=result)
-
-@app.route('/resta', methods=['GET', 'POST'])
-def resta():
-    result = None
-    if request.method == 'POST':
-        dato1 = request.form.get('dato1')
-        dato2 = request.form.get('dato2')
-        if dato1 and dato2:
-            try:
-                num1 = float(dato1)
-                num2 = float(dato2)
+            elif action == 'restar':
                 result = num1 - num2
-            except ValueError:
-                result = "ingresar solo numeros"
-    return render_template('resta.html', result=result)
-
-@app.route('/multiplicacion', methods=['GET', 'POST'])
-def multiplicacion():
-    result = None
-    if request.method == 'POST':
-        dato1 = request.form.get('dato1')
-        dato2 = request.form.get('dato2')
-        if dato1 and dato2:
-            try:
-                num1 = float(dato1)
-                num2 = float(dato2)
+            elif action == 'multiplicar':
                 result = num1 * num2
-            except ValueError:
-                result = "ingresar solo numeros"
-    return render_template('multiplicacion.html', result=result)
+        except ValueError:
+            result = "ingresar solo numeros"
+    return render_template('operacion.html', result=result, operacion=action)
 
 if __name__ == '__main__':
     app.run(debug=True)
